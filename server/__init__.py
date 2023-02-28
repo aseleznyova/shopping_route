2a51707c6c7991b78738e3ac92a40cc252845051
aseleznyova <nastya.seleznyovaa@gmail.com>
2023-02-28 21:35:49 +0300

    added main requests

--- server/__init__.py
+++ server/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     flask_app = Flask(__name__)
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_CONNECTION_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    flask_app.secret_key = 'super secret key'
     flask_app.app_context().push()
     db.init_app(flask_app)
     db.create_all()
--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 import json
-from flask import request
-from __init__ import create_app
+from flask import request, session
+from __init__ import create_app, get_dist
 import database
 from models import Products, Stores, StoresProduct
 
@@ -14,80 +14,46 @@ def fetch_products():
         new_product = {
             "id": product.id,
             "name": product.name,
-            "price": product.price,
             "weight": product.weight
         }
-
         all_products.append(new_product)
     return json.dumps(all_products), 200
 
 @app.route('/products', methods=['POST'])
-def add_product():
+def add_list_proucts():
     data = request.get_json()
-    name = data['name']
-    price = data['price']
-    weight = data['weight']
+    session['list_products'] = []
+    for item in data:
+        if Products.query.get(item):
+            session['list_products'].append(item)
+    return json.dumps("Added"), 201
 
-    database.add_instance(Products, name=name, price=price, weight=weight)
-    return json.dumps("Added"), 200
-
-@app.route('/stores', methods=['GET'])
-def fetch_stores():
-    stores = database.get_all(Stores)
-    all_stores = []
-    for store in stores:
-        new_store = {
-            "id": store.id,
-            "name": store.name,
-            "short_name": store.short_name
-        }
-
-        all_stores.append(new_store)
-    return json.dumps(all_stores), 200
-
-@app.route('/stores', methods=['POST'])
-def add_store():
+@app.route('/coordinates', methods=['POST'])
+def add_coordinates():
     data = request.get_json()
-    name = data['name']
-    short_name = data['short_name']
+    session['coordinates'] = {
+        'longitude' : data['longitude'],
+        'latitude' : data['latitude']
+    }
+    return json.dumps("Added"), 201
 
-    database.add_instance(Stores, name=name, short_name=short_name)
-    return json.dumps("Added"), 200
-
-@app.route('/product_stores', methods=['GET'])
-def fetch_products_stores():
-    products_stores = database.get_all(StoresProduct)
-    all_products_stores = []
-    for products_store in products_stores:
-        new_product = {
-            "stores_id": products_store.stores_id,
-            "products_id": products_store.products_id,
-        }
-
-        all_products_stores.append(new_product)
-    return json.dumps(all_products_stores), 200
-
-@app.route('/product_stores', methods=['POST'])
-def add_products_stores():
+@app.route('/coordinates', methods=['DELETE'])
+def delete_coordinates():
     data = request.get_json()
-    stores_id = data['stores_id']
-    products_id = data['products_id']
-
-    database.add_instance(StoresProduct, stores_id=stores_id, products_id=products_id)
-    return json.dumps("Added"), 200
-
-
-@app.route('/remove/<product_id>', methods=['DELETE'])
-def remove(product_id):
-    database.delete_instance(Product, id=product_id)
+    session['coordinates'] = {}
     return json.dumps("Deleted"), 200
 
-@app.route('/edit/<product_id>', methods=['PATCH'])
-def edit(product_id):
-    data = request.get_json()
-    new_price = data['price']
-    database.edit_instance(Product, id=product_id, price=new_price)
-    return json.dumps("Edited"), 200
+@app.route('/products', methods=['DELETE'])
+def delete_list_proucts():
+    session['list_products'] = []
+    return json.dumps("Deleted"), 200
+
+@app.route('/route', methods=['GET'])
+def building_route():
+    if 'coordinates' in session and 'list_products' in session:    
+        return json.dumps('correct'), 200
+    return json.dumps('coordinates or a list of products are missing'), 401
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 5001)
